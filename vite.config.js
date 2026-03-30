667c9cd56bb196a3a7c1720787ddfd6a57f2e081
Blackfire363 <blackfire363@yahoo.co.uk>
2026-03-30 21:13:49 +0100

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/gta-police-calculator/',
   plugins: [react(), tailwindcss()],
 })
